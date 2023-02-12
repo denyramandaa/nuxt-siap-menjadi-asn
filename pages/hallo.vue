67aa25dc055f3a0f30f01884543288934827ee17
Myoui Mina <ramandadeny7@gmail.com>
2023-02-12 11:34:49 +0700

    [Init] Nuxt 3 Project

---
+++ pages/hallo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-full min-h-screen bg-slate-900 flex justify-center items-center text-white">
+        Hallo, Deny Ramanda
+    </div>
+</template>
+<script setup>
+    definePageMeta({
+        middleware: 'auth',
+        title: 'Hallo Page'
+    })
+    const route = useRoute()
+    console.log(route)
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full min-h-screen bg-slate-900 flex justify-center items-center text-white">
+        Welcome to Index, Deny Ramanda
+    </div>
+</template>
